64fb179bd5bdc25dab2fc1fdb9a48e3a247171e8
csxuen <jessicacsx0118@gmail.com>
2026-07-01 21:47:03 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,69 @@
+const initialBoard = {
+  "r_rook1":"a10",
+  "r_horse1":"b10",
+  "r_elephant1":"c10",
+  "r_advisor1":"d10",
+  "r_general":"e10",
+  "r_advisor2":"f10",
+  "r_elephant2":"g10",
+  "r_horse2":"h10",
+  "r_rook2":"i10",
+
+  "r_cannon1":"b8",
+  "r_cannon2":"h8",
+
+  "r_soldier1":"a7",
+  "r_soldier2":"c7",
+  "r_soldier3":"e7",
+  "r_soldier4":"g7",
+  "r_soldier5":"i7",
+
+  "b_rook1":"a1",
+  "b_horse1":"b1",
+  "b_elephant1":"c1",
+  "b_advisor1":"d1",
+  "b_general":"e1",
+  "b_advisor2":"f1",
+  "b_elephant2":"g1",
+  "b_horse2":"h1",
+  "b_rook2":"i1",
+
+  "b_cannon1":"b3",
+  "b_cannon2":"h3",
+
+  "b_soldier1":"a4",
+  "b_soldier2":"c4",
+  "b_soldier3":"e4",
+  "b_soldier4":"g4",
+  "b_soldier5":"i4"
+};
+
+function clone(board) {
+  return JSON.parse(JSON.stringify(board));
+}
+
+function findPiece(board, pos) {
+  for (let p in board) {
+    if (board[p] === pos) return p;
+  }
+  return null;
+}
+
+function applyMove(board, from, to) {
+  const newBoard = clone(board);
+
+  const piece = findPiece(newBoard, from);
+  if (!piece) return { ok: false, board: newBoard };
+
+  const captured = findPiece(newBoard, to);
+  if (captured) delete newBoard[captured];
+
+  newBoard[piece] = to;
+
+  return { ok: true, board: newBoard };
+}
+
+module.exports = {
+  initialBoard,
+  applyMove
+};
